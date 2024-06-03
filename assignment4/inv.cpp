9e246c04b3d851ed0fdf659bffcfc4e271efa96c
RaduTek <radutek13@gmail.com>
2024-06-03 13:22:32 +0300

    inv good

--- assignment4/inv.cpp
+++ assignment4/inv.cpp
@@ -1,99 +1,82 @@
-#include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
-
-class SegmentTree
-{
-private:
-    unsigned n;
-    int *data;
-
-public:
-    SegmentTree(unsigned items)
-    {
-        n = items;
-        data = new int[4 * n];
-        if (data == NULL)
-        {
-            perror("Memory allocation error");
-            return;
-        }
-        fill(data, data + 4 * n, 0);
-    }
-
-    ~SegmentTree()
-    {
-        delete[] data;
-    }
-
-    void update(unsigned index, int value, unsigned node, unsigned left, unsigned right)
-    {
-        if (left == right)
-        {
-            data[node] += value;
-            return;
-        }
-
-        unsigned middle = (left + right) / 2;
-        if (index <= middle)
-            update(index, value, 2 * node, left, middle);
-        else
-            update(index, value, 2 * node + 1, middle + 1, right);
-
-        data[node] = max(data[2 * node], data[2 * node + 1]);
-    }
-
-    int query(unsigned node, unsigned left, unsigned right, unsigned qLeft, unsigned qRight)
-    {
-        if (qLeft <= left && right <= qRight)
-            return data[node];
-
-        if (qLeft > right || qRight < left)
-            return 0;
-
-        unsigned middle = (left + right) / 2;
-        int lQuery = query(2 * node, left, middle, qLeft, qRight);
-        int rQuery = query(2 * node + 1, middle + 1, right, qLeft, qRight);
-
-        return lQuery + rQuery;
-    }
-
-    int inversions(int start)
-    {
-        return query(1, 1, n, start + 1, n);
-    }
-};
 
 ifstream fin("inv.in");
 ofstream fout("inv.out");
 
+int mergeCount(int *S, int left, int mid, int right)
+{
+    int LSlen = mid - left + 1;
+    int *LS = new int[LSlen];
+    for (int i = 0; i < LSlen; ++i)
+        LS[i] = S[left + i];
+
+    int RSlen = right - mid;
+    int *RS = new int[RSlen];
+    for (int i = 0; i < RSlen; ++i)
+        RS[i] = S[mid + 1 + i];
+
+    int i = 0, j = 0, k = left;
+    int count = 0;
+
+    while (i < LSlen && j < RSlen)
+    {
+        if (LS[i] <= RS[j])
+            S[k] = LS[i++];
+        else
+        {
+            // Inversion condition matches
+            S[k] = RS[j++];
+            count += LSlen - i;
+        }
+        k++;
+    }
+
+    while (i < LSlen)
+        S[k++] = LS[i++];
+
+    while (j < RSlen)
+        S[k++] = RS[j++];
+
+    // fix memory leak
+    delete[] LS;
+    delete[] RS;
+
+    return count;
+}
+
+int countInversions(int *S, int left, int right)
+{
+    if (left >= right)
+        return 0;
+
+    int mid = (left + right) / 2;
+
+    int invCount = countInversions(S, left, mid);
+    invCount += countInversions(S, mid + 1, right);
+    invCount += mergeCount(S, left, mid, right);
+
+    return invCount;
+}
+
 int main()
 {
     unsigned n;
-    int x;
+    unsigned count = 0;
 
     fin >> n;
 
-    SegmentTree st(n);
-    int *original = new int[n];
-    unsigned count = 0;
-
-    // Read items into segment tree
+    int *S = new int[n];
     for (unsigned i = 0; i < n; i++)
-    {
-        fin >> x;
-        original[i] = x;
+        fin >> S[i];
 
-        st.update(i, x, 1, 1, n);
-
-        count += st.inversions(i);
-    }
+    count = countInversions(S, 0, n - 1);
 
     fout << count % 9917 << endl;
 
     fin.close();
     fout.close();
-
     return 0;
 }
